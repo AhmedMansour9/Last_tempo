0102b253a40ac1c70f8f600677a3e3b984cc6b0e
Ahmed Mansour <37387373+AhmedMansour9@users.noreply.github.com>
2020-05-30 19:09:47 +0200

    ;;

---
+++ app/src/main/java/com/tempomena/Activites/Blocked.java
@@ -0,0 +1,16 @@
+package com.tempomena.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tempomena.R;
+
+public class Blocked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocked);
+    }
+}
